729ad133de6d6392b16398de45442cc0d12815fd
k98 <golamrabbani.dsi@gmail.com>
2019-02-12 15:38:28 +0600

    Add knex

--- connection.js
+++ connection.js
@@ -1,9 +1,17 @@
-const mysql = require('mysql');
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+});
 
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "password",
   database:'library'
 });
 
@@ -12,4 +20,14 @@ conn.connect(function(err) {
   console.log('connected')
 });
 
+      knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    return knex.schema.createTable('users', function(t) {
+      t.increments('id').primary();
+      t.string('first_name', 100);
+      t.string('last_name', 100);
+      t.text('bio');
+    });
+  }
+});
 module.exports = conn;
